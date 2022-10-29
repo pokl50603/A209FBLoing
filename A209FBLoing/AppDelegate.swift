19862368491f25be2a3ac2c38e238c1084e022b8
shengyuan wu <pokl50603@gmail.com>
2022-10-29 17:06:41 +0800

    singIn

--- A209FBLoing/AppDelegate.swift
+++ A209FBLoing/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Firebase
+import FacebookLogin
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,8 +16,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        ApplicationDelegate.shared.application(
+                    application,
+                    didFinishLaunchingWithOptions: launchOptions
+                )
         return true
+    }
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
     }
 
     // MARK: UISceneSession Lifecycle
